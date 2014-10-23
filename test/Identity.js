00226018f5aaab086b422235f3672c36f3e37417
Esteban Ordano <eordano@gmail.com>
2014-10-23 17:26:32 -0300

    Mocha tests fixed

--- test/Identity.js
+++ test/Identity.js
@@ -349,7 +349,7 @@ describe('Identity model', function() {
     });
 
     it('should create an encrypted object', function() {
-      var ret = JSON.parse(iden.export());
+      var ret = JSON.parse(iden.exportAsJson());
       ret.iterations.should.equal(13);
       ret.profile.should.equal('penc');
       _.each([0, 1, 2, 3, 4], function(i) {
@@ -380,7 +380,7 @@ describe('Identity model', function() {
 
 
     it('should check the import string', function(done) {
-      Identity.import(JSON.stringify({
+      Identity.importFromJson(JSON.stringify({
         profile: '1234'
       }), '1234', config, function(err, ret) {
         err.should.contain('BADSTR');
@@ -390,7 +390,7 @@ describe('Identity model', function() {
 
 
     it('should check the import string 2', function(done) {
-      Identity.import(JSON.stringify({
+      Identity.importFromJson(JSON.stringify({
         iterations: 10,
       }), '1234', config, function(err, ret) {
         err.should.contain('BADSTR');
@@ -399,7 +399,7 @@ describe('Identity model', function() {
     });
 
     it('should import a simple wallet', function(done) {
-      Identity.import(JSON.stringify({
+      Identity.importFromJson(JSON.stringify({
         iterations: 10,
         profile: '1234'
       }), '1234', config, function(err, iden) {
