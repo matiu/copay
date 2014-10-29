e9df2cbd9fc4bed51d73dca4142c39afb7315f03
Esteban Ordano <eordano@gmail.com>
2014-10-29 15:29:06 -0300

    Export the crypto utils package for copay-mobile

--- copay.js
+++ copay.js
@@ -15,6 +15,7 @@ module.exports.Identity = require('./js/models/Identity');
 module.exports.Wallet = require('./js/models/Wallet');
 module.exports.Compatibility = require('./js/models/Compatibility');
 module.exports.PluginManager = require('./js/models/PluginManager');
+module.exports.cryptoUtils = require('./js/util/crypto');
 module.exports.version = require('./version').version;
 module.exports.commitHash = require('./version').commitHash;
 
