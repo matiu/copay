f3acad7832718b78bcf8280e35d876d3ea7b12f1
Matias Pando <mpando@bitpay.com>
2015-04-08 10:15:23 -0300

    Rm console.log and comments and minor label changes

--- src/js/controllers/preferences.js
+++ src/js/controllers/preferences.js
@@ -15,56 +15,10 @@ angular.module('copayApp.controllers').controller('preferencesController',
     this.unitName = config.wallet.settings.unitName;
     this.bwsurl = config.bws.url;
 
-
     this.selectedAlternative = {
       name: config.wallet.settings.alternativeName,
       isoCode: config.wallet.settings.alternativeIsoCode
     };
-
-    console.log('this.unitName ', this.unitName);
-    console.log('this.selectedAlternative  ', this.selectedAlternative);
-
-
-
-
-    // var self = this;
-    // rateService.whenAvailable(function() {
-    //   self.alternativeOpts = rateService.listAlternatives();
-    //   for (var ii in self.alternativeOpts) {
-    //     if (config.wallet.settings.alternativeIsoCode === self.alternativeOpts[ii].isoCode) {
-    //       self.selectedAlternative = self.alternativeOpts[ii];
-    //     }
-    //   }
-    //   $scope.$digest();
-    // });
-
-
-    // this.save = function() {
-    //   var opts = {
-    //     wallet: {
-    //       settings: {
-    //         unitName: this.selectedUnit.shortName,
-    //         unitToSatoshi: this.selectedUnit.value,
-    //         unitDecimals: this.selectedUnit.decimals,
-    //         unitCode: this.selectedUnit.code,
-    //         alternativeName: this.selectedAlternative.name,
-    //         alternativeIsoCode: this.selectedAlternative.isoCode,
-    //       }
-    //     },
-    //     bws: {
-    //       url: this.bwsurl,
-    //     }
-    //   };
-
-    //   configService.set(opts, function(err) {
-    //     if (err) console.log(err);
-    //     var hardRestart = !$scope.settingsForm.bwsurl.$pristine;
-    //     applicationService.restart(hardRestart);
-    //     go.walletHome();
-    //     $scope.$emit('Local/ConfigurationUpdated');
-    //     notification.success('Success', $filter('translate')('settings successfully updated'));
-    //   });
-    // };
 
     var _modalDeleteWallet = function() {
       var ModalInstanceCtrl = function($scope, $modalInstance) {
